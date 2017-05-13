16bc58447c41067384b447ae0be701d6f021c700
Michelle Gienow <michellegienow@gmail.com>
2017-05-13 10:12:51 -0400

    refactoring var names for correct syntax

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 (function(){
-
-var counter = 0, // to keep track of current slide
-    $items = document.querySelectorAll('.diy-slideshow figure'), // a collection of all of the slides, caching for performance
-    numItems = $items.length; // total number of slides
+// to keep track of current slide:
+var counter = 0,
+    items = document.querySelectorAll('.diySlideshow figure'), // a collection of all of the slides, caching for performance
+    numItems = items.length; // total number of slides
 
 // this function is what cycles the slides, showing the next or previous slide and hiding all the others
 var showCurrent = function(){
@@ -10,12 +10,12 @@ var showCurrent = function(){
 
   // remove .show from whichever element currently has it
   // http://stackoverflow.com/a/16053538/2006057
-  [].forEach.call( $items, function(el){
+  [].forEach.call( items, function(el){
     el.classList.remove('show');
   });
 
   // add .show to the one item that's supposed to have it
-  $items[itemToShow].classList.add('show');
+  items[itemToShow].classList.add('show');
 };
 
 // add click events to prev & next buttons
@@ -29,4 +29,4 @@ document.querySelector('.prev').addEventListener('click', function() {
      showCurrent();
   }, false);
 
-})();  
+})();
