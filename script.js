f2e80c4d8297d7d8f67746afaa958ebeeff0f7d9
Michelle Gienow <michellegienow@gmail.com>
2017-05-15 10:37:18 -0400

    refactor JS and adjust variable names across project

--- script.js
+++ script.js
@@ -1,20 +1,20 @@
 (function(){
 // to keep track of current slide:
-var counter = 0,
-    items = document.querySelectorAll('.diySlideshow figure'), // a collection of all of the slides, caching for performance
-    numItems = items.length; // total number of slides
+var counter = 0;
+var items = document.querySelectorAll('.minimalistSlideshow figure'); // a collection of all of the slides, caching for performance
+var numItems = items.length; // total number of slides
 
 // showCurrent is how we cycle the slides, showing next or previous slide while **hiding all the others
 var showCurrent = function(){
   var itemToShow = Math.abs(counter%numItems);// use modulo operator to get actual index of the element to show
 
-  // remove .show class from whichever element currently holds that designation
+  // Now remove .show class from whichever element currently holds that designation
   // http://stackoverflow.com/a/16053538/2006057
   [].forEach.call( items, function(el){
     el.classList.remove('show');
   });
 
-  // add .show to the one item that's supposed to have it
+  // Now add .show to the one item that's supposed to have it
   items[itemToShow].classList.add('show');
 };
 
